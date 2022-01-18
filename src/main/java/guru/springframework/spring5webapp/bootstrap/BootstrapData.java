478fa4724375d153411e6eb981d6608bc1125540
pa-os <patrick.oeffentlich@gmail.com>
2022-01-18 19:15:59 +0100

    Add component to bootstrap some data.
    Lesson 17.

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,43 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Slf4j
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+  private final AuthorRepository authorRepository;
+
+  private final BookRepository bookRepository;
+
+  @Override
+  public void run(String... args) {
+
+    Author eric = new Author("Eric", "Evans");
+    Book ddd = new Book("Domain Driven Design", "123123");
+    eric.getBooks().add(ddd);
+    ddd.getAuthors().add(eric);
+
+    authorRepository.save(eric);
+    bookRepository.save(ddd);
+
+    Author rod = new Author("Rod", "Kohnson");
+    Book noEJB = new Book("J2EE Development without EJB", "2346341");
+    eric.getBooks().add(noEJB);
+    ddd.getAuthors().add(rod);
+
+    authorRepository.save(rod);
+    bookRepository.save(noEJB);
+
+    log.info("Started in Bootstrap");
+    log.info("Number of Books: " + bookRepository.count());
+  }
+}
