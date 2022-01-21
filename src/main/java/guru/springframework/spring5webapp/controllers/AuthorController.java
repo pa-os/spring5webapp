74de0896642c47bcd85f0bfe17dcb9ea2a164b3c
pa-os <patrick.oeffentlich@gmail.com>
2022-01-21 16:33:45 +0100

    Add AuthorController and a template for the author list.
    Assignment 3.

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,23 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping(path="/authors")
+@Controller
+public class AuthorController {
+
+  private final AuthorRepository authorRepository;
+
+  @GetMapping
+  public String getAuthors(Model model) {
+    model.addAttribute("authors", authorRepository.findAll());
+    return "authors/list";
+  }
+
+}
